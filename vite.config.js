cc195df6b30877b58c1c046d728742d711b3c8bb
aboode95 <abdullamasud95@gmail.com>
2024-12-03 12:34:57 +0300

    Initial commit.

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@models': path.resolve(__dirname, '/home/abdulla/Documents/Github Documents/edu_vr/Test (Study)/models'),
+      "@damn": path.resolve(__dirname, '/home/abdulla/Documents/Github\ Documents/edu_vr/Test\ (Study)/Test04\ -\ Click\ Orbit\ Blender\ Box/Box.glb'),
+    },
+  },
+  server: {
+    port: 3000, // Optionally change the port if you need to
+    fs: {
+      // Allow serving files from parent directory
+      allow: ['..'],
+    }
+  }
+});
